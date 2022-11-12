7390d8a1a4eebec24ccfaeab3a3f36e076091b4d
BartaBalazs00 <77434768+BartaBalazs00@users.noreply.github.com>
2022-11-12 23:55:07 +0100

    Romboló tud rombolni és építeni is

---
+++ ElsoFelevesFeladat/Negyzet.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElsoFelevesFeladat
+{
+    class Negyzet
+    {
+        private int[,] negyzet;
+
+        public Negyzet(int oszlopokSzama, int sorokSzama)
+        {
+            this.negyzet = new int[oszlopokSzama, sorokSzama];
+            UjJatek();
+            
+        }
+        public void UjJatek()
+        {
+            int szelesseg = this.negyzet.GetLength(0) + 2;
+            int magassag = this.negyzet.GetLength(0) + 4;
+            Console.SetWindowSize(szelesseg, magassag);
+            NegyzetFeltoltese();
+        }
+        public int NegyzetMagassaga()
+        {
+            return this.negyzet.GetLength(0);
+        }
+        public int NegyzetHossza()
+        {
+            return this.negyzet.GetLength(1);
+        }
+        //megnézi, hogy üres-e a négyzet
+        public bool UresEANegyzet()
+        {
+            for (int i = 0; i < this.negyzet.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.negyzet.GetLength(1); j++)
+                {
+                    if (this.negyzet[i,j] !=0 )
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        //megnézi, hogy tele van-e a négyzet
+        public bool TeleVanEANegyzet()
+        {
+            for (int i = 0; i < this.negyzet.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.negyzet.GetLength(1); j++)
+                {
+                    if (this.negyzet[i, j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        //feltölti a négyzeteket random számokkal 1 és 4 között
+        private void NegyzetFeltoltese()
+        {
+            Random random = new Random();
+            for (int i = 0; i < this.negyzet.GetLength(0) / 2; i++)
+            {
+                for (int j = 0; j < this.negyzet.GetLength(1); j++)
+                {
+                    this.negyzet[i, j] = random.Next(1, 5);
+                    //this.negyzet[i, j] = 0;
+                    
+                }
+            }
+            for (int i = this.negyzet.GetLength(0) / 2; i < this.negyzet.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.negyzet.GetLength(1); j++)
+                {
+                    this.negyzet[i, j] = 0;
+                }
+            }
+        }
+        //kiirajzolja a négyzetet a random számok segítségével
+        public void NegyzetKirajzolasa(int szinSzama, string jatekos)
+        {
+            Console.Clear();
+            Console.ForegroundColor = (ConsoleColor)szinSzama;
+            Console.WriteLine(jatekos);
+            Console.ResetColor();
+            Console.Write("+");
+            for (int i = 0; i < this.negyzet.GetLength(1); i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+            for (int i = 0; i < this.negyzet.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.negyzet.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write("|");
+                    }
+                    Console.BackgroundColor = (ConsoleColor)this.negyzet[i, j];
+                    Console.Write(" ");
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("|");
+            }
+            Console.Write("+");
+            for (int i = 0; i < this.negyzet.GetLength(1); i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+        }
+        public void NegyzetEpitese(int oszlopSzam, int szinSzama)
+        {
+            int index = negyzet.GetLength(0) - 1;
+            while (index >= 0 && negyzet[index, oszlopSzam] == 0)
+            {
+                index--;
+            }
+            if (index != negyzet.GetLength(0)-1)
+            {
+                negyzet[index + 1, oszlopSzam] = szinSzama;
+            }
+            
+        }
+        
+        public void NegyzetRombolasa(int oszlopSzam, int szinSzama)
+        {
+            int sorszam = negyzet.GetLength(0) - 1;
+            while (sorszam > 0 && negyzet[sorszam, oszlopSzam] == 0)
+            {
+                sorszam--;
+            }
+            bool vanEKorulotteUgyanOlyanSzin = false;
+            if (negyzet[sorszam, oszlopSzam] == szinSzama)//ha megegyezik a talált szín
+            {
+                vanEKorulotteUgyanOlyanSzin = true;
+                negyzet[sorszam, oszlopSzam] = 0;
+                //ha a negyzet legalján van akkor ott romboljon, ha nem akkor alatta eggyel és ugyan ott
+                if (sorszam == this.NegyzetMagassaga()-1)
+                {
+                    RombolasKorulotte(sorszam, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                }
+                else
+                {
+                    RombolasKorulotte(sorszam + 1, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                    RombolasKorulotte(sorszam, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                }
+                
+            }
+            else if(negyzet[sorszam, oszlopSzam] != szinSzama && (sorszam != this.NegyzetMagassaga() - 1 && sorszam != 0))
+            //ha nem egyezik meg a színszama és nem a matrix tetején vagy alján van akkor a megtalált szín fölött rombol
+            {
+                RombolasKorulotte(sorszam + 1, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+            }
+            else if (negyzet[sorszam, oszlopSzam] != szinSzama && sorszam == 0)
+            //ha nem egyezik meg a szinszama de a matrix tetején, helyezkedik el
+            {
+                if (negyzet[sorszam, oszlopSzam] != 0)// megnézzük, hogy ott szín van vagy már ki van lőve
+                {
+                    RombolasKorulotte(sorszam + 1, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                }
+                else
+                {
+                    RombolasKorulotte(sorszam, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                }
+            }
+            if (!vanEKorulotteUgyanOlyanSzin)//ha nem talált és nem volt körülötte ugyan olyan szín
+            {
+                if (sorszam != this.NegyzetMagassaga()-1)
+                {
+                    if (sorszam == 0)
+                    {
+                        if (negyzet[sorszam, oszlopSzam] != 0)// megnézzük, hogy ott szín van vagy már ki van lőve
+                        {
+                            negyzet[sorszam + 1, oszlopSzam] = szinSzama;
+                        }
+                        else
+                        {
+                            negyzet[sorszam, oszlopSzam] = szinSzama;
+                        }
+                    }
+                    else
+                    {
+                        negyzet[sorszam + 1, oszlopSzam] = szinSzama;
+                    }
+                }
+            }
+        }
+        public void RombolasKorulotte(int sorszam, int oszlopSzam, int szinSzama, ref bool vanEKorulotteUgyanOlyanSzin)
+        {
+            if (sorszam - 1 >= 0)
+            {
+                if (negyzet[sorszam - 1, oszlopSzam] == szinSzama)//megnézzük, hogy felette van e ugyan olyan szín
+                {
+                    negyzet[sorszam - 1, oszlopSzam] = 0;
+                    vanEKorulotteUgyanOlyanSzin = true;
+                    if (sorszam != 0)
+                    {
+                        RombolasKorulotte(sorszam - 1, oszlopSzam,  szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                    }
+                    
+                }
+            }
+
+            if (oszlopSzam - 1 >= 0)
+            {
+                if (negyzet[sorszam, oszlopSzam - 1] == szinSzama)//megnézzük, hogy balra van e ugyan olyan szín
+                {
+                    negyzet[sorszam, oszlopSzam - 1] = 0;
+                    vanEKorulotteUgyanOlyanSzin = true;
+                    if (oszlopSzam != 0)
+                    {
+                        RombolasKorulotte(sorszam, oszlopSzam - 1, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                    }
+                }
+            }
+
+            if (oszlopSzam + 1 <= negyzet.GetLength(1) - 1)
+            {
+                if (negyzet[sorszam, oszlopSzam + 1] == szinSzama)//megnézzük, hogy jobbra van e ugyan olyan szín
+                {
+                    negyzet[sorszam, oszlopSzam + 1] = 0;
+                    vanEKorulotteUgyanOlyanSzin = true;
+                    if (oszlopSzam != negyzet.GetLength(1) - 1)
+                    {
+                        RombolasKorulotte(sorszam, oszlopSzam + 1, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                    }
+                }
+            }
+
+            if (sorszam + 1 <= negyzet.GetLength(0) - 1)
+            {
+                if (negyzet[sorszam + 1, oszlopSzam] == szinSzama)//megnézzük, hogy alatta van e ugyan olyan szín
+                {
+                    negyzet[sorszam + 1, oszlopSzam] = 0;
+                    vanEKorulotteUgyanOlyanSzin = true;
+                    if (sorszam != negyzet.GetLength(0) - 1)
+                    {
+                        RombolasKorulotte(sorszam + 1, oszlopSzam, szinSzama, ref vanEKorulotteUgyanOlyanSzin);
+                    }
+                }
+            }
+            
+        }
+    }
+}
--- ElsoFelevesFeladat/Program.cs
+++ ElsoFelevesFeladat/Program.cs
@@ -8,151 +8,126 @@ namespace ElsoFelevesFeladat
 {
     class Program
     {
-        //feltölti a négyzeteket random számokkal 1 és 4 között
-        public static void NegyzetFeltoltese(ref int[,] negyzet)
-        {
-            Random random = new Random();
-            for (int i = 0; i < negyzet.GetLength(0) / 2; i++)
-            {
-                for (int j = 0; j < negyzet.GetLength(1); j++)
-                {
-                    negyzet[i, j] = random.Next(1, 5);
-                }
-            }
-            for (int i = negyzet.GetLength(0) / 2; i < negyzet.GetLength(0); i++)
-            {
-                for (int j = 0; j < negyzet.GetLength(1); j++)
-                {
-                    negyzet[i, j] = 0;
-                }
-            }
-        }
-        //kiirajzolja a négyzetet a random számok segítségével
-        public static void NegyzetKirajzolasa(int[,] negyzet, int szinSzama)
-        {
-            Console.Clear();
-            Console.ForegroundColor = (ConsoleColor)szinSzama;
-            Console.WriteLine("Romboló");
-            Console.ResetColor();
-            Console.Write("+");
-            for (int i = 0; i < negyzet.GetLength(1); i++)
-            {
-                Console.Write("-");
-            }
-            Console.WriteLine("+");
-            for (int i = 0; i < negyzet.GetLength(0); i++)
-            {
-                for (int j = 0; j < negyzet.GetLength(1); j++)
-                {
-                    if (j == 0)
-                    {
-                        Console.Write("|");
-                    }
-                    Console.BackgroundColor = (ConsoleColor)negyzet[i, j];
-                    Console.Write(" ");
-                }
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine("|");
-            }
-            Console.Write("+");
-            for (int i = 0; i < negyzet.GetLength(1); i++)
-            {
-                Console.Write("-");
-            }
-            Console.WriteLine("+");
-        }
+        
+        
         //mozgatja a cursort jobbra vagy balra
-        public static void SetCursorPosition(ref int positionWidth, int minusOrPlus, int positionHeight, int szinSzama)
+        public static void SetCursorPosition(ref int positionWidth, int minusOrPlus, int positionHeight)
         {
             positionWidth += minusOrPlus;
             Console.SetCursorPosition(positionWidth, positionHeight);
         }
-        public static void NegyzetRombolasa(ref int[,] negyzet, int oszlopszam, int szinSzama)
+        public static int Mozgas(int positionWidth, int positionHeight, Negyzet negyzet)
         {
-            int index = negyzet.GetLength(0) - 1;
-            while (index >= 0 && negyzet[index, oszlopszam] == 0)
+            Console.SetCursorPosition(positionWidth, positionHeight);
+            bool space = false;
+            while (!space)
             {
-                index--;
-            }
-            RombolasKorulotte(ref negyzet, oszlopszam, index + 1, szinSzama);
-        }
-        public static void RombolasKorulotte(ref int[,] negyzet, int oszlopszam, int sorszam, int szinSzama)
-        {
-            if (sorszam-1 >= 0)
-            {
-                if (negyzet[sorszam - 1, oszlopszam] == szinSzama)//megnézzük, hogy felette van e ugyan olyan szín
+                ConsoleKey theKey = Console.ReadKey().Key;
+                while (theKey != ConsoleKey.LeftArrow && theKey != ConsoleKey.RightArrow && theKey != ConsoleKey.Spacebar)//Amíg nem a balra, jobbra vagy a space nyilat nyomja le
                 {
-                    negyzet[sorszam - 1, oszlopszam] = 0;
-                    if (sorszam != 0)
-                    {
-                        RombolasKorulotte(ref negyzet, oszlopszam, sorszam - 1, szinSzama);
-                    }
-                    NegyzetKirajzolasa(negyzet, szinSzama);
+                    SetCursorPosition(ref positionWidth, 0, positionHeight);
+                    theKey = Console.ReadKey().Key;
                 }
-            }
-
-            if (oszlopszam-1 >= 0)
-            {
-                if (negyzet[sorszam, oszlopszam - 1] == szinSzama)//megnézzük, hogy balra van e ugyan olyan szín
+                if (theKey == ConsoleKey.LeftArrow)//Ha balra mozog
                 {
-                    negyzet[sorszam, oszlopszam - 1] = 0;
-                    if (oszlopszam != 0)
+                    if (positionWidth > 1)//ha még tud balra mozogni
                     {
-                        RombolasKorulotte(ref negyzet, oszlopszam - 1, sorszam, szinSzama);
+                        SetCursorPosition(ref positionWidth, -1, positionHeight);
                     }
-                    NegyzetKirajzolasa(negyzet, szinSzama);
+                    else//ha már nem tud balra mozogni
+                    {
+                        SetCursorPosition(ref positionWidth, 0, positionHeight);
+                    }
                 }
-            }
-
-            if (oszlopszam+1 <= negyzet.GetLength(1)-1)
-            {
-                if (negyzet[sorszam, oszlopszam + 1] == szinSzama)//megnézzük, hogy jobbra van e ugyan olyan szín
+                else if (theKey == ConsoleKey.RightArrow)//ha jobbra mozog
                 {
-                    negyzet[sorszam, oszlopszam + 1] = 0;
-                    if (oszlopszam != negyzet.GetLength(1) - 1)
+                    if (positionWidth < negyzet.NegyzetHossza())//ha még tud jobbra mozogni
                     {
-                        RombolasKorulotte(ref negyzet, oszlopszam + 1, sorszam, szinSzama);
+                        SetCursorPosition(ref positionWidth, +1, positionHeight);
                     }
-                    NegyzetKirajzolasa(negyzet, szinSzama);
+                    else//ha már nem tud jobbra mozogni
+                    {
+                        SetCursorPosition(ref positionWidth, 0, positionHeight);
+                    }
                 }
-            }
-
-            if (sorszam+1 <= negyzet.GetLength(0)-1)
-            {
-                if (negyzet[sorszam + 1, oszlopszam] == szinSzama)//megnézzük, hogy alatta van e ugyan olyan szín
+                else if (theKey == ConsoleKey.Spacebar)
                 {
-                    negyzet[sorszam + 1, oszlopszam] = 0;
-                    if (sorszam != negyzet.GetLength(0) - 1)
-                    {
-                        RombolasKorulotte(ref negyzet, oszlopszam, sorszam + 1, szinSzama);
-                    }
-                    NegyzetKirajzolasa(negyzet, szinSzama);
+                    space = true;
                 }
             }
             
+            return positionWidth - 1;
         }
+        
         static void Main(string[] args)
         {
-            //bekérjük, hogy mekkora legyen a a játéknak a tere
-            Console.WriteLine("Add meg, hogy milyen nagy legyen a jaták 6-20 közötti páros szám");
-            string nagysagString = Console.ReadLine();
-            int nagysag = 0;
-            //addig csináljuk ameddig nem számot, vagy nem 5 és 20 közötti számot adott meg
-            while (!int.TryParse(nagysagString, out nagysag) || (nagysag < 6 || nagysag >20) || (nagysag % 2 == 1))
+            bool ujJatek = true;
+            while (ujJatek)
             {
-                Console.WriteLine("nem számot adtál meg, vagy nem a megfelelő intervallumon belül. Add meg újra");
-                nagysagString = Console.ReadLine();
-            }
-            Console.Clear();
-            Console.WriteLine("Romboló");//még változtatni kell
-            //beállítjuk a beírt nagység függvényében a játékteret, kirajzoljuk a négyzetet és 
-            int[,] negyzet = new int[nagysag,nagysag];
-            int width = negyzet.GetLength(0)+2;
-            int height = negyzet.GetLength(0) + 4;
-            Console.SetWindowSize(width, height);
-            NegyzetFeltoltese(ref negyzet);
+                Console.WriteLine("Add meg, hogy milyen nagy legyen a jaták 6-20 közötti páros szám");
+                string nagysagString = Console.ReadLine();
+                int nagysag = 0;
+                //addig csináljuk ameddig nem számot, vagy nem 5 és 20 közötti számot adott meg
+                while (!int.TryParse(nagysagString, out nagysag) || (nagysag < 2 || nagysag > 20) || (nagysag % 2 == 1))
+                {
+                    Console.WriteLine("nem számot adtál meg, vagy nem a megfelelő intervallumon belül. Add meg újra");
+                    nagysagString = Console.ReadLine();
+                }
+                Console.Clear();
+                Negyzet negyzet = new Negyzet(nagysag, nagysag);
 
-            Random random = new Random();
+                int positionWidth = negyzet.NegyzetHossza() / 2 + 1;
+                int positionHeight = negyzet.NegyzetMagassaga() + 3;
+                Random random = new Random();
+                int szinSzama = random.Next(1, 5);
+                int oszlopSzam;
+            
+                while (!negyzet.TeleVanEANegyzet() && !negyzet.UresEANegyzet())
+                {
+                    // az építő jön
+                    /*
+                    positionWidth = negyzet.NegyzetHossza() / 2 + 1;
+                    positionHeight = negyzet.NegyzetMagassaga() + 3;
+                    szinSzama = random.Next(1, 5);
+                    negyzet.NegyzetKirajzolasa(szinSzama, "Építő");
+                    oszlopSzam = Mozgas(positionWidth, positionHeight, negyzet);
+                    negyzet.NegyzetEpitese(oszlopSzam, szinSzama);
+                    */
+                    //romboló jön
+                    positionWidth = negyzet.NegyzetHossza() / 2 + 1;
+                    positionHeight = negyzet.NegyzetMagassaga() + 3;
+                    szinSzama = random.Next(1, 5);
+                    negyzet.NegyzetKirajzolasa(szinSzama, "Romboló");
+                    oszlopSzam = Mozgas(positionWidth, positionHeight, negyzet);
+                    negyzet.NegyzetRombolasa(oszlopSzam, szinSzama);
+                }
+                if (negyzet.TeleVanEANegyzet())
+                {
+                    Console.Clear();
+                    Console.SetWindowSize(40, 7);
+                    Console.WriteLine("Az építő nyert");
+                }
+                else if (negyzet.UresEANegyzet())
+                {
+                    Console.Clear();
+                    Console.SetWindowSize(40, 7);
+                    Console.WriteLine("A romboló nyert!");
+                }
+                Console.WriteLine("szeretnétek mégegyet játszani? I/N");
+                string szeretnenekMegJatszani = Console.ReadLine().Trim();
+                while (szeretnenekMegJatszani.ToLower() != "i" && szeretnenekMegJatszani.ToLower() != "n")
+                {
+                    Console.WriteLine("Nem jól adtad meg a válaszodat. Add meg mégyszer (I/N)");
+                    szeretnenekMegJatszani = Console.ReadLine();
+                }
+                if (szeretnenekMegJatszani.ToLower() == "n")
+                {
+                    ujJatek = false;
+                }
+            }
+
+            /*
+            
             int szinSzama = random.Next(1, 5);
             NegyzetKirajzolasa(negyzet, szinSzama);
 
@@ -163,45 +138,9 @@ namespace ElsoFelevesFeladat
             while (true)
             {
                 NegyzetKirajzolasa(negyzet, szinSzama);
-                Console.SetCursorPosition(positionWidth, positionHeight);
-                ConsoleKey theKey = Console.ReadKey().Key;
-                while (theKey != ConsoleKey.LeftArrow && theKey != ConsoleKey.RightArrow && theKey != ConsoleKey.Spacebar)//Amíg nem a balra, jobbra vagy a space nyilat nyomja le
-                {
-                    SetCursorPosition(ref positionWidth, 0, positionHeight, szinSzama);
-                    theKey = Console.ReadKey().Key;
-                }
-                if (theKey == ConsoleKey.LeftArrow)//Ha balra mozog
-                {
-                    if (positionWidth > 1)//ha még tud balra mozogni
-                    {
-                        SetCursorPosition(ref positionWidth, -1, positionHeight, szinSzama);
-                    }
-                    else//ha már nem tud balra mozogni
-                    {
-                        SetCursorPosition(ref positionWidth, 0, positionHeight, szinSzama);
-                    }
-                    
-                }
-                else if(theKey == ConsoleKey.RightArrow)//ha jobbra mozog
-                {
-                    if (positionWidth < negyzet.GetLength(1))//ha még tud jobbra mozogni
-                    {
-                        SetCursorPosition(ref positionWidth, +1, positionHeight, szinSzama);
-                    }
-                    else//ha már nem tud jobbra mozogni
-                    {
-                        SetCursorPosition(ref positionWidth, 0, positionHeight, szinSzama);
-                    }
-                }
-                else if (theKey == ConsoleKey.Spacebar)
-                {
-                    NegyzetRombolasa(ref negyzet, positionWidth - 1, szinSzama);
-                    positionWidth = negyzet.GetLength(1) / 2 + 1;
-                    positionHeight = negyzet.GetLength(1) + 3;
-                    SetCursorPosition(ref positionWidth, 0, positionHeight, szinSzama);
-                    szinSzama = random.Next(1, 5);
-                }
+                
             }
+            */
             Console.ReadKey();
         }
     }
